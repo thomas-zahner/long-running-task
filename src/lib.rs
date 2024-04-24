56c21f999ab608e0365bb3efed44aeccd79b1455
Thomas Zahner <thomas.zahner@protonmail.ch>
2024-04-24 14:38:38 +0200

    Add documentation and attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,45 @@
+//! `long-running-task` is a simple library to handle and manage [long-running tasks](https://restfulapi.net/rest-api-design-for-long-running-tasks/).
+//! If you want to use this crate in combination with web-frameworks you probably want to enable the feature `serde`.
+
+#![warn(clippy::all, clippy::pedantic)]
+#![warn(missing_docs)]
+
 use std::{
     collections::HashMap,
     time::{Duration, Instant},
 };
 use uuid::Uuid;
 
+/// Structs implementing this trait hold the current progress of a task.
 pub trait Progressible {
+    /// Report progress on a task, for example by increasing a progress field by 1.
     fn progress(&mut self);
 }
 
 #[derive(PartialEq, Eq, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize))]
-pub enum TaskState<T, P>
+/// Representation of a task's state.
+pub enum TaskState<V, P>
 where
     P: Progressible,
 {
+    /// The task is not finished yet and holds its current progress.
     Pending(P),
-    Done(T),
+    /// The task is done with a value of type V.
+    Done(V),
 }
 
-pub struct TaskPool<T, P>
+/// A pool to manage long-running tasks.
+pub struct TaskPool<V, P>
 where
     P: Progressible,
 {
     pending: HashMap<Uuid, P>,
-    completed: HashMap<Uuid, (Instant, T)>,
+    completed: HashMap<Uuid, (Instant, V)>,
     lifespan: Option<Duration>,
 }
 
-impl<T, P> Default for TaskPool<T, P>
+impl<V, P> Default for TaskPool<V, P>
 where
     P: Progressible,
 {
@@ -47,27 +59,33 @@ pub struct Handle {
     uuid: Uuid,
 }
 
-impl<T, P> TaskPool<T, P>
+impl<V, P> TaskPool<V, P>
 where
     P: Progressible + Clone,
 {
     /// Configure the lifespan of tasks.
     /// `None` means that tasks will never expire.
     /// Expired tasks are purged as soon as `complete` is invoked.
+    /// Specifying a lifespan is useful because clients might not always retrieve completed tasks.
+    /// Without configuring a lifespan, such abandoned tasks accumulate over time and fill up memory.
     #[cfg(feature = "lifespan")]
+    #[must_use]
     pub fn with_lifespan(mut self, lifespan: Option<Duration>) -> Self {
         self.lifespan = lifespan;
         self
     }
 
+    /// Insert a task's initial progress to receive a `Handle` and `Uuid`
+    /// referencing the task.
+    #[must_use]
     pub fn insert(&mut self, pending: P) -> (Handle, Uuid) {
         let uuid = Uuid::new_v4();
         self.pending.insert(uuid, pending);
         (Handle { uuid }, uuid)
     }
 
-    /// Get the task state and remove it from the pool if it is done
-    pub fn retrieve(&mut self, uuid: &Uuid) -> Option<TaskState<T, P>> {
+    /// Get the task state and remove it from the pool if it is done.
+    pub fn retrieve(&mut self, uuid: &Uuid) -> Option<TaskState<V, P>> {
         use TaskState::{Done, Pending};
 
         if let Some(p) = self.pending.get(uuid) {
@@ -77,17 +95,20 @@ where
         self.completed.remove(uuid).map(|f| Done(f.1))
     }
 
+    /// Report progress on a pending task.
+    /// Calls `Progressible::progress` on the corresponding progress state.
     pub fn progress(&mut self, handle: &Handle) {
-        self.pending
-            .get_mut(&handle.uuid)
-            .expect("Pending task not found. This should never happen because a task's handle cannot outlive the task.")
-            .progress();
+        match self.pending.get_mut(&handle.uuid) {
+            Some(p) => p.progress(),
+            None => unreachable!("Pending task not found. This should never happen because a task's handle cannot outlive the task."),
+        }
     }
 
     /// Mark the task associated to the handle as completed with a value of type T.
     /// The handle must be passed by value so that this is the final action.
     /// As a side effect expired tasks are purged.
-    pub fn complete(&mut self, handle: Handle, value: T) {
+    #[allow(clippy::needless_pass_by_value)]
+    pub fn complete(&mut self, handle: Handle, value: V) {
         self.pending.remove(&handle.uuid);
         self.purge_expired_tasks();
         self.completed.insert(handle.uuid, (Instant::now(), value));
@@ -193,7 +214,7 @@ mod tests {
         id
     }
 
-    fn get_inner_size<T, P>(pool: &TaskPool<T, P>) -> usize
+    fn get_inner_size<V, P>(pool: &TaskPool<V, P>) -> usize
     where
         P: Progressible,
     {
